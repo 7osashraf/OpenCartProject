e3a560f1272a058002687e6939c76060a0015e4d
Ahmed Saad <a7med.saad238@gmail.com>
2024-10-21 20:07:57 +0300

    Ahmed-Saad-Khalifa

--- src/test/java/LoginPage/LoginPageWebElements.java
+++ src/test/java/LoginPage/LoginPageWebElements.java
@@ -1,4 +1,17 @@
 package LoginPage;
 
-public class LoginPageWebElements {
+import java.io.IOException;
+import org.openqa.selenium.WebElement;
+import Utility.FindingElements;
+
+public class LoginPageWebElements extends FindingElements {
+
+    // Get Email field
+    public WebElement GetEmail() throws IOException {return findElementById("input-email");}
+
+    // Get Password field
+    public WebElement GetPassword() throws IOException {return findElementById("input-password");}
+
+    //  Get Login Button Contianer
+    public WebElement GetContinueButton() throws IOException {return findElementByClassName("btn-primary");}
 }
--- src/test/java/RegisterPage/RegisterPageWebElements.java
+++ src/test/java/RegisterPage/RegisterPageWebElements.java
@@ -1,4 +1,31 @@
 package RegisterPage;
 
-public class RegisterPageWebElements {
+import Utility.FindingElements;
+import org.openqa.selenium.WebElement;
+
+import java.io.IOException;
+
+public class RegisterPageWebElements extends FindingElements {
+
+    // Get FirstName field
+    public WebElement GetFirstName() throws IOException {return findElementById("input-firstname");}
+
+    // Get LastName field
+    public WebElement GetLastName() throws IOException {return findElementById("input-lastname");}
+
+    // Get Email field
+    public WebElement GetEmail() throws IOException {return findElementById("input-email");
+    }
+
+    // Get Telephone field
+    public WebElement GetTelephone() throws IOException {return findElementById("input-telephone");}
+
+    // Get Password field
+    public WebElement GetPassword() throws IOException {return findElementById("input-password");}
+
+    // Get Password Confirm field
+    public WebElement GetPasswordConfirm() throws IOException {return findElementById("input-confirm");}
+
+    //  Get Continue Button Container
+    public WebElement GetContinueButton() throws IOException {return findElementByClassName("btn-primary");}
 }
