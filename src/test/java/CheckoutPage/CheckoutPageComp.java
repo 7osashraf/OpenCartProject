6f149198683c57826f391c62eb8c2a7f0687b7ef
7osashraf <hosashraf70@gmail.com>
2024-10-21 23:47:38 +0300

    add method in CheckoutPageComp and test it

--- src/test/java/CheckoutPage/CheckoutPageComp.java
+++ src/test/java/CheckoutPage/CheckoutPageComp.java
@@ -1,6 +1,10 @@
 package CheckoutPage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class CheckoutPageComp extends CheckoutPageWebElement{
 
@@ -8,12 +12,95 @@ public class CheckoutPageComp extends CheckoutPageWebElement{
         logger.debug("Verify if the checkout complete page is displayed.");
 
         String url = driver.getCurrentUrl();
-        checkIfEqual(url, "https://demo.opencart.com/en-gb?route=checkout/checkout");
+        checkIfEqual(url, "https://tutorialsninja.com/demo/index.php?route=checkout/checkout");
 
         // chcek page title
         WebElement checkoutPageTitle = getcheckoutPageTitle();
         checkIfNotNull(checkoutPageTitle, "Checkout page");
         CheckIfTextIsDiplayed(checkoutPageTitle, "Checkout");
+    }
+
+    public void checkIfCheckoutOptions(){
+        logger.debug("Verify if the checkout options is displayed and can select guest user and continue.");
+
+        List<WebElement> panels = getPanels();
+        WebElement firstPanel = panels.get(0);
+
+        CheckIfTextIsDiplayed(firstPanel, "Step 1: Checkout Options ");
+        WebElement panelTitle = firstPanel.findElement(By.className("panel-title"));
+        String titleText = panelTitle.getText();
+        System.out.println(titleText);
+
+        checkIfEqual(titleText, "Step 1: Checkout Options");
+
+        WebElement guestCheckoutOption = driver.findElement(By.xpath("//input[@value='guest']"));
+        guestCheckoutOption.click();
+
+        WebElement continueBtn = getContinueBtn1();
+        continueBtn.click();
+
+    }
+
+    public void validateBillingDetails() {
+        logger.debug("check that we can fill the inputs sections in the step 2 in checkout page");
+
+        WebElement billingDetailsForm = getBillingDetailsForm();
+        explicitlyWaitForVisibility(billingDetailsForm);
+        checkIfNotNull(billingDetailsForm, "Billing details form");
+
+        fillAndVerifyField(getFirstNameField(), "Hos");
+        fillAndVerifyField(getLastNameField(), "ashraf");
+        fillAndVerifyField(getEmailField(), "hosashraf@gmail.com");
+        fillAndVerifyField(getTelephoneField(), "0123456789");
+        fillAndVerifyField(getAddress1Field(), "10 saad");
+        fillAndVerifyField(getCityField(), "Alex");
+        fillAndVerifyField(getPostcodeField(), "220755");
+
+        selectAndVerifyField(getCountryField(),"Egypt");
+        selectAndVerifyField(getZoneField(),"Al Buhayrah");
+
+        WebElement stepTwoContinueBtn = getContinueBtn2();
+        stepTwoContinueBtn.click();
+    }
+
+
+    public void checkIfSelectingCheckoutOptions(){
+        WebElement guestCheckoutSelect = getGuestCheckoutSelect();
+        guestCheckoutSelect.click();
+
+        WebElement continueBtn = getContinueBtn1();
+        continueBtn.click();
+    }
+
+//    public void checkIfSelectingPaymentMethod() {
+//        WebElement paymentMethod = getPaymentMethodElement();
+//        paymentMethod.click();
+//        WebElement continueBtn = getPaymentContinueBtn();
+//        continueBtn.click();
+//    }
+
+
+    public void helpFunc(){
+        WebElement addToCartBtn = findElementById("button-cart");
+        addToCartBtn.click();
+
+        WebElement checkoutBtn = findElementByXPath("//*[@id=\"top-links\"]/ul/li[5]/a/span");
+        checkoutBtn.click();
+    }
+
+    private void fillAndVerifyField(WebElement field, String value) {
+        field.clear();
+        field.sendKeys(value);
+        String enteredValue = field.getAttribute("value");
+        checkIfEqual(enteredValue, value);
+    }
+
+    private void selectAndVerifyField(WebElement dropdown, String value) {
+        Select select = new Select(dropdown);
+        select.selectByVisibleText(value);
+        WebElement selectedOption = select.getFirstSelectedOption();
+        String selectedValue = selectedOption.getText();
+        checkIfEqual(selectedValue, value);
     }
 
 }
--- src/test/java/CheckoutPage/CheckoutPageWebElement.java
+++ src/test/java/CheckoutPage/CheckoutPageWebElement.java
@@ -3,7 +3,25 @@ package CheckoutPage;
 import Utility.FindingElements;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class CheckoutPageWebElement extends FindingElements {
 
     public WebElement getcheckoutPageTitle() {return findElementById("content");}
+    public WebElement getGuestCheckoutSelect(){return findElementByXPath("/html/body/div[2]/div/div/div/div[1]/div[2]/div/div/div[1]/div[2]/label/input");}
+    public WebElement getContinueBtn1(){return findElementById("button-account");}
+    public WebElement getContinueBtn2(){return findElementById("button-guest");}
+    public WebElement getBillingDetailsForm(){return findElementById("accordion");}
+    public WebElement getFirstNameField(){return findElementById("input-payment-firstname");}
+    public WebElement getLastNameField(){return findElementById("input-payment-lastname");}
+    public WebElement getEmailField(){return findElementById("input-payment-email");}
+    public WebElement getTelephoneField(){return findElementById("input-payment-telephone");}
+    public WebElement getAddress1Field(){return findElementById("input-payment-address-1");}
+    public WebElement getCityField(){return findElementById("input-payment-city");}
+    public WebElement getPostcodeField(){return findElementById("input-payment-postcode");}
+    public WebElement getCountryField(){return findElementById("input-payment-country");}
+    public WebElement getZoneField(){return findElementById("input-payment-zone");}
+
+    public List<WebElement> getPanels() {return findElementsByClassName("panel-default");}
+
 }
