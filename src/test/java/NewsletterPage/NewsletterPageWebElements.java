17ab2a7487bda373c3ab6dfebabdec8d985c1acc
Marina <marina.nz@gmail.com>
2024-10-23 08:05:20 +0200

    Marina Newsletter and Brands pages

--- src/test/java/NewsletterPage/NewsletterPageWebElements.java
+++ src/test/java/NewsletterPage/NewsletterPageWebElements.java
@@ -5,14 +5,10 @@ import org.openqa.selenium.WebElement;
 
 public class NewsletterPageWebElements extends FindingElements {
 
-//    public WebElement GetProductContentContainer(){return findElementByClassName("caption"); }
-//    public WebElement GetProductImage(){ return findElementByClassName("image");}
-//    public WebElement GetProductName(){return findElementByTagName("h4");}
-//    public WebElement GetAddToCartButton(){return findElementByXPath("//*[@id=\"content\"]/div[2]/div[1]/div/div[3]/button[1]");}
-//    public WebElement GetAddToWishListButton(){return findElementByXPath("//*[@id=\"content\"]/div[2]/div[1]/div/div[3]/button[2]");}
-//    public WebElement GetHeaderWishListIcon(){return findElementByXPath("//*[@id=\"wishlist-total\"]");}
-//    public WebElement GetCompareThisProductButton(){return findElementByXPath("//*[@id=\"content\"]/div[2]/div[1]/div/div[3]/button[3]");}
-//    public WebElement GetHeaderCartIcon(){return findElementByClassName("btn-inverse");}
-//    public WebElement GetAlert(){return findElementByClassName("alert");}
-
+      public WebElement GetNewsletterContainer(){return findElementById("account-newsletter");}
+      public WebElement GetContinueBtn(){return findElementByXPath("//button[text()='Continue']");}
+      public WebElement GetBackBtn() {return findElementByXPath("//a[text()='Back']");}
+      public WebElement GetTitle(){return findElementByTagName("h1");}
+      public WebElement GetYesBtn(){return findElementByXPath("//input[@value='1']");}
+      public WebElement GetNoBtn(){return findElementByXPath("//input[@value='0']");}
 }
--- src/test/java/ReturnsPage_Test/ReturnsPageCompTest.java
+++ src/test/java/ReturnsPage_Test/ReturnsPageCompTest.java
@@ -1,6 +1,13 @@
 package ReturnsPage_Test;
 
 import ReturnsPage.ReturnsPageComp;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 
 public class ReturnsPageCompTest extends ReturnsPageComp {
+
+    @AfterMethod
+    public void Close(ITestResult result) throws Exception {
+        closeDriver(result);
+    }
 }
