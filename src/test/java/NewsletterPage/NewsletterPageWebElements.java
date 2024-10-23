62925d10524c295ba713bee354228574eaaf3687
7osashraf <hosashraf70@gmail.com>
2024-10-23 13:47:23 +0300

    finish

--- src/test/java/NewsletterPage/NewsletterPageWebElements.java
+++ src/test/java/NewsletterPage/NewsletterPageWebElements.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 public class NewsletterPageWebElements extends FindingElements {
 
       public WebElement GetNewsletterContainer(){return findElementById("account-newsletter");}
-      public WebElement GetContinueBtn(){return findElementByXPath("//button[text()='Continue']");}
+      public WebElement GetContinueBtn(){return findElementByXPath("//input[@value='Continue']");}
       public WebElement GetBackBtn() {return findElementByXPath("//a[text()='Back']");}
       public WebElement GetTitle(){return findElementByTagName("h1");}
       public WebElement GetYesBtn(){return findElementByXPath("//input[@value='1']");}
